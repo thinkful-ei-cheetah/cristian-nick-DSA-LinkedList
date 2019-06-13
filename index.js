9da3ae750d38f1253496e89baf881fc299eb1382
Nick Lee <leexnick@gmail.com>
2019-06-13 12:10:38 -0700

    3-4

--- index.js
+++ index.js
@@ -134,24 +134,125 @@ class LinkedList {
 
 function main() {
   const SLL = new LinkedList()
-
+  
   SLL.insertFirst('Apollo');
   SLL.insertLast('Boomer');
   SLL.insertLast('Helo');
   SLL.insertLast('Husker');
   SLL.insertLast('Starbuck');
-
+  
   SLL.insertLast('Tauhida');
-
+  
   // returns item not found
   // SLL.remove('squirrel') 
-
+  
   SLL.insertBefore('Athena', 'Boomer')
   SLL.insertAfter('Hotdog', 'Helo')
-
+  
   SLL.insertAt('Kat', 3)
   
   SLL.remove('Tauhida')
+
+  return SLL
 }
 
-main();
+const LL = main();
+
+function display(LL) {
+  let output = ''
+
+  let currNode = LL.head;
+  
+  while(currNode !== null){
+    output += currNode.val
+
+    if(currNode.next !== null) {
+      output += ' -> '
+    }
+
+    currNode = currNode.next
+  }
+
+  return output
+}
+
+console.log(display(LL))
+
+function size(LL) {
+  let size = 0
+
+  let currNode = LL.head
+
+  while(currNode !== null) {
+    size++
+    currNode = currNode.next
+  }
+
+  return size
+}
+
+const emptyLL = new LinkedList();
+// console.log(size(emptyLL))
+// console.log(size(LL))
+
+function isEmpty(LL) {
+  if(LL.head === null) {
+    return true
+  }
+
+  return false
+}
+
+// console.log(isEmpty(emptyLL))
+// console.log(isEmpty(LL))
+
+function findPrevious(item, LL) {
+  if(LL.head === null) {
+    console.log(`linked list is empty!`)
+    return;
+  }
+
+  let currNode = LL.head;
+  let prevNode = LL.head;
+
+
+  while((currNode !== null) && (currNode.val !== item)) {
+    prevNode = currNode;
+    currNode = currNode.next;
+  }
+
+  if(currNode === null) {
+    console.log('Item not found');
+    return;
+  }
+
+  return console.log(prevNode.val);
+}
+
+// findPrevious('Kat', LL)
+// findPrevious('Kat', emptyLL)
+// findPrevious('Falsy', LL)
+
+function findLast(LL) { //[1-> 2 -> 3]
+  if(LL.head === null) {
+    return 'linked list is empty';
+  }
+  
+  let currNode = LL.head;
+
+  while(currNode.next !== null) {
+    currNode = currNode.next
+  }
+
+  return currNode.val
+}
+
+// console.log(findLast(LL))
+// console.log(findLast(emptyLL))
+
+// Input = 1->2->3
+// (1) current = 1
+//     newNode = 1
+//     newNode.next = 2
+
+// 4. Removes duplicates in a Linked List with O(n^2)
